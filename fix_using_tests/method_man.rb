9bed9c43e0f7adf9fd5b338d04bc1fcc19937d9d
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-01 21:52:18 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,8 +2,12 @@ require 'pry'
 
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
+  binding.pry
+  puts 'hello'
 end
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
+  binding.pry
+  puts 'hello'
 end
